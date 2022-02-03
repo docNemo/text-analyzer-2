be22f9b175b0c8f2a88ff7a3166db359bb1f4740
Урубков Владислав Станиславович <19826347@sberbank.ru>
2022-02-03 11:56:41 +0300

    Fixed magic const

--- src/main/java/com/training/Formatter.java
+++ src/main/java/com/training/Formatter.java
@@ -5,6 +5,12 @@ import com.training.Exceptions.WriteException;
 
 public class Formatter {
     private static final byte NUM_SPACES = 4;
+    private static final char SPACE = ' ';
+    private static final char OPENING_BRACE = '{';
+    private static final char CLOSING_BRACE = '}';
+    private static final char SEMICOLON = ';';
+    private static final char NEW_LINE = '\n';
+
     IReader reader;
     IWriter writer;
 
@@ -22,25 +28,25 @@ public class Formatter {
             char character = reader.readChar();
 
             switch (character) {
-                case '\n' -> {
+                case NEW_LINE -> {
                     //ignore
                 }
-                case ' ' -> {
+                case SPACE -> {
                     if (wasWord) {
                         wasWord = false;
                     }
                 }
-                case '{' -> {
+                case OPENING_BRACE -> {
                     writeOpeningBrace(newLine, wasWord, nestingLevel);
                     newLine = true;
                     nestingLevel++;
                 }
-                case '}' -> {
+                case CLOSING_BRACE -> {
                     writeClosingBrace(newLine, nestingLevel);
                     nestingLevel--;
                     newLine = true;
                 }
-                case ';' -> {
+                case SEMICOLON -> {
                     writeSemicolon();
                     newLine = true;
                 }
@@ -56,41 +62,41 @@ public class Formatter {
     public void writeIndent(int nestingLevel) throws WriteException {
         int size_indent = NUM_SPACES * nestingLevel;
         while (size_indent > 0) {
-            writer.writeChar(' ');
+            writer.writeChar(SPACE);
             size_indent--;
         }
     }
 
     public void writeOpeningBrace(boolean newLine, boolean wasWord, int nestingLevel) throws WriteException {
         if (wasWord) {
-            writer.writeChar(' ');
+            writer.writeChar(SPACE);
         }
         if (newLine) {
             writeIndent(nestingLevel);
         }
-        writer.writeChar('{');
-        writer.writeChar('\n');
+        writer.writeChar(OPENING_BRACE);
+        writer.writeChar(NEW_LINE);
     }
 
     public void writeClosingBrace(boolean newLine, int nestingLevel) throws WriteException {
         if (!newLine) {
-            writer.writeChar('\n');
+            writer.writeChar(NEW_LINE);
         }
         writeIndent(nestingLevel - 1);
-        writer.writeChar('}');
-        writer.writeChar('\n');
+        writer.writeChar(CLOSING_BRACE);
+        writer.writeChar(NEW_LINE);
     }
 
     public void writeSemicolon() throws WriteException {
-        writer.writeChar(';');
-        writer.writeChar('\n');
+        writer.writeChar(SEMICOLON);
+        writer.writeChar(NEW_LINE);
     }
 
     public void writeCommonChar(boolean newLine, boolean wasWord, int nestingLevel, char character) throws WriteException {
         if (newLine) {
             writeIndent(nestingLevel);
         } else if (!wasWord) {
-            writer.writeChar(' ');
+            writer.writeChar(SPACE);
         }
         writer.writeChar(character);
     }
